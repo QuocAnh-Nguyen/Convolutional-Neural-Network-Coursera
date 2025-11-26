6f27d0a06d85a980a7c37aadc50e46049a6d2f16
QuocAnh <quocanhaddc@gmail.com>
2025-11-27 00:55:41 +0700

    Added README.md and W3A1 test.py

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a repo about my CNN course on Coursera
---
+++ W3A1/test.py
@@ -0,0 +1,14 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import seaborn as sns   
+import tensorflow as tf
+
+# %%
+print(np.array([1,2,3]))
+# %% [markdown]
+# # This is a markdown cell
+# ## Table of contents
+# - [1.1 - Section 1](#section1)
+# - [Section 2](#section2)
